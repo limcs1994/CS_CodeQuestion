091f34013e725803084cd48f90cbdfcb73ae760d
LimChinSiang <chin.siang.lim@invantest.com>
2024-04-15 18:32:10 +0800

    Add file

---
+++ Q3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_CodeQuestion
+{
+    /*
+   Print fibonacci using recursive
+
+  Example
+  Output: 0,1,1,2,3,5,8,13,21, ...
+   */
+
+    internal class Q3
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(fibonacci(i).ToString() + ", ");
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int fibonacci(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return fibonacci(n - 1) + fibonacci(n - 2);
+            }
+        }
+    }
+}
---
+++ Q4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_CodeQuestion
+{
+    /*
+   Reverse a string
+
+   Example
+    input: "Hello world"
+    Output: "dlrow olleh"
+  */
+
+    internal class Q4
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "AbcDefGhIJ";
+            StringBuilder strBuilder = new StringBuilder(inputString);
+            strBuilder.
+
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                strBuilder.Append(inputString[i]);
+            }
+        }
+    }
+}
